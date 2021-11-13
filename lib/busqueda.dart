6578c82baf81d91b3b735db9225ff90103bd1230
Leonardo Valderrama <leonardov7@gmail.com>
2021-11-12 20:33:55 -0500

    Se agregó comentario pubspec

--- lib/busqueda.dart
+++ lib/busqueda.dart
@@ -1,1 +1,18 @@
-//
+import 'package:flutter/material.dart';
+
+class busqueda extends StatefulWidget{
+  @override
+  BusquedaApp createState() => BusquedaApp();
+}
+
+class BusquedaApp extends State<busqueda>{
+  @override
+  Widget build(BuildContext context){
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Resultado de la búsqueda"),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -11,46 +11,63 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+  return MaterialApp(debugShowCheckedModeBanner:false, home:Index());
+  }
+}
+class Index extends StatefulWidget{
+  @override
+  IndexStart createState() => IndexStart();
+}
+
+@override
+class IndexStart extends State<Index>{
+  Widget build(BuildContext context){
     return MaterialApp(
         title: 'AppMovil',
         home: Scaffold(
-            appBar: AppBar(
-              title: Text('Home'),
-            ),
-            body: SingleChildScrollView(
-              child: Column(
-                children: <Widget>[
-                  Padding(padding: EdgeInsets.only(left: 25,top: 25,right: 25,bottom: 0),
-                    child: Center(
-                      child:Container(
-                        width: 100,
-                        height: 100,
-                        child: Image.asset('image/logo.png'),
-                      ),
-                    ) ,
-                  ),
-                  Padding(padding: EdgeInsets.only(left: 25,top: 25,right: 25,bottom: 0),
-                    child: TextField(
-                      decoration: InputDecoration(
-                        border: OutlineInputBorder(),
-                          labelText:'Búsqueda',
-                          hintText:'Palabra clave de la búsqueda'
-                        ),
-                      ),
+          appBar: AppBar(
+            title: Text('Home'),
+          ),
+          body: SingleChildScrollView(
+            child: Column(
+              children: <Widget>[
+                Padding(padding: EdgeInsets.only(left: 25,top: 25,right: 25,bottom: 0),
+                  child: Center(
+                    child:Container(
+                      width: 100,
+                      height: 100,
+                      child: Image.asset('image/logo.png'),
                     ),
-                  Padding(padding: EdgeInsets.only(top: 20),
-                  child:ElevatedButton(onPressed: (){
-                    print("Presionó el botón");
-                   // Navigator.push(context,
-                       // MaterialPageRoute(builder: (_) => busqueda()));
-                  },
-                    child: Text('Buscar'),
-                  )
+                  ) ,
+                ),
+                Padding(padding: EdgeInsets.only(left: 25,top: 25,right: 25,bottom: 0),
+                  child: TextField(
+                    decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        labelText:'Búsqueda',
+                        hintText:'Palabra clave de la búsqueda'
+                    ),
                   ),
-                ],
-              ),
+                ),
+                Padding(padding: EdgeInsets.only(top: 20),
+                    child:ElevatedButton(onPressed: (){
+                      print("Presionó el botón");
+                      Navigator.push(context,
+                          MaterialPageRoute(builder: (_) => busqueda()));
+                    },
+                      child: Text('Buscar'),
+                    )
+                ),
+              ],
             ),
+          ),
         )
     );
+
   }
+
 }
+
+
+
+
