73d9f49aa6f992f8b9caf4802e338a60f86be1ae
Leonardo Valderrama <leonardov7@gmail.com>
2021-11-22 21:20:14 -0500

    Gestion usuario

--- lib/Tiendas/ShopList.dart
+++ lib/Tiendas/ShopList.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 class ShopList extends StatefulWidget {
@@ -7,55 +8,84 @@ class ShopList extends StatefulWidget {
 
 class ShopListApp extends State<ShopList> {
   @override
-  String texto1="Comidas rápidas el Gordo --";
+  String texto1 = "Comidas rápidas el Gordo --";
+
+  /// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
   Widget build(BuildContext context) {
-    Widget titleSection = Container(
-      padding: const EdgeInsets.all(15),
-      child: Row(
-        children: [
-          Expanded(
-            /*1*/
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                /*2*/
-                Container(
-                  padding: const EdgeInsets.only(bottom: 8),
-                  child: Text(
-                    texto1,
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                ),
-                Text(
-                  'las mejores comidas del barrio',
-                  style: TextStyle(
-                    color: Colors.grey[500],
-                  ),
-                ),
-              ],
-            ),
-          ),
-          /*3*/
-         Container(
-           width: 70,
-           height: 70,
-           child: Image.asset("image/comida-rapida.png"),
-         ),
-         ElevatedButton(onPressed: (){}, child: Text("Entrar"))
-        ],
-      ),
-    );
+    ///++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
     return Scaffold(
       appBar: AppBar(
         title: Text("Lista de tiendas"),
       ),
-      body: ListView(
-        children: [
-          titleSection,
-          titleSection
-        ],
+      body: Container(
+        child: Center(
+          child: StreamBuilder(
+            stream:
+                FirebaseFirestore.instance.collection("Tiendas").snapshots(),
+            builder:
+                (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+              if (!snapshot.hasData) return CircularProgressIndicator();
+              return ListView.builder(
+                  itemCount: snapshot.data!.docs.length,
+                  itemBuilder: (BuildContext context, int index) {
+                    return new Card(
+                      child: Column(
+                        children: [
+                          Container(
+                            padding: const EdgeInsets.all(15),
+                            child: Row(
+                              children: [
+                                Expanded(
+                                  /*1*/
+                                  child: Column(
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    children: [
+                                      /*2*/
+                                      Container(
+                                        padding:
+                                            const EdgeInsets.only(bottom: 8),
+                                        child: Text(
+                                          snapshot.data!.docs[index]
+                                              .get("nombreTienda"),
+                                          style: TextStyle(
+                                            fontWeight: FontWeight.bold,
+                                          ),
+                                        ),
+                                      ),
+                                      Text(
+                                      snapshot.data!.docs[index]
+                                          .get("descripcion"),
+                                        style: TextStyle(
+                                          color: Colors.grey[500],
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                                /*3*/
+                                Container(
+                                  width: 70,
+                                  height: 70,
+                                  child: Image.asset("image/"+snapshot.data!.docs[index]
+                                      .get("rutaFoto"),),
+                                ),
+                                ElevatedButton(
+                                    onPressed: () {
+                                      snapshot.data!.docs[index]
+                                          .id;
+                                    }, child: Text("Entrar"))
+                              ],
+                            ),
+                          )
+                        ],
+                      ),
+                    );
+                  });
+            },
+          ),
+        ),
       ),
     );
   }
